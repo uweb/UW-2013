d893c031408b0a476fde91c8daf61c015ac83bc5
Dane Odekirk <dizzzy@uw.edu>
2013-09-13 15:38:51 -0700

    slideshow images can link to external urls

--- inc/widgets/slideshow.php
+++ inc/widgets/slideshow.php
@@ -10,10 +10,19 @@
  *  to fetch the attachments from the backend. (JS for fetching on the admin side and PHP for fetching 
  *  on the public side)
  *
+ * [TODO]: CSS is being used for now to hide the custom 'External Link' field from showing up in the 
+ *          Media Library outside of the slideshow.
+ *
  */
 
 class UW_Slideshow extends WP_Widget
 {
+
+  public $ALLOWED_SCREENS = array( 
+    'widgets',
+    'appearance_page_so_panels_home_page'
+  );
+
   function UW_Slideshow()
   {
     parent::WP_Widget( 
@@ -27,8 +36,11 @@ class UW_Slideshow extends WP_Widget
 
     if ( is_admin() ) 
     {
-      add_action('admin_enqueue_scripts', array( __CLASS__, 'scripts') );
-      add_action('admin_print_styles', array( __CLASS__, 'styles' ) );
+      add_action( 'admin_enqueue_scripts', array( $this , 'scripts') );
+      add_action( 'admin_print_styles', array( $this , 'styles' ) );
+      add_filter( 'attachment_fields_to_edit', array( $this , 'external_link_to_field_edit' ), 101, 2 );
+		  add_filter( 'attachment_fields_to_save', array( $this , 'external_link_to_field_save' ), null , 2 );
+      
     }
   }
 
@@ -38,12 +50,15 @@ class UW_Slideshow extends WP_Widget
     wp_enqueue_media();
   }
 
-  /**
-   * Bug fix: some form elements in the media library aren't clickable and conflict with the panels page builder
-   */
   function styles() 
   {
+    $screen = get_current_screen();
+
+    //Bug fix: some form elements in the media library aren't clickable and conflict with the panels page builder
     echo '<style type="text/css"> .media-sidebar { z-index:0 !important; } </style>';
+
+    if ( ! in_array( $screen->id, $this->ALLOWED_SCREENS ))
+      echo '<style type="text/css"> .compat-field-external_link { display:none !important; }; </style>';
   }
 
   function form( $instance ) 
@@ -61,20 +76,20 @@ class UW_Slideshow extends WP_Widget
 
     <p>
 
-      <ul class="slideshow-preview"> </ul>
+      <ul class="slideshow-preview" style="float:left;width:100%;"></ul>
 
-      <input id="<?php echo $this->get_field_id( 'slideshow' ); ?>" name="<?php echo $this->get_field_name( 'slideshow' ); ?>" type="text" value="<?php echo esc_attr( $slideshow ); ?>"/>
+      <input id="<?php echo $this->get_field_id( 'slideshow' ); ?>" name="<?php echo $this->get_field_name( 'slideshow' ); ?>" type="hidden" value="<?php echo esc_attr( $slideshow ); ?>"/>
 
       <a class="select-a-gallery button" href="#">Select Images</a>
 
     </p>
 
     <script type="text/template" id="tmpl-slideshow-images">
-      <li class="attachment selection selected save-ready" style="width:40px;">
-        <div class="attachment-preview type-image subtype-png {{ data.sizes.thumbnail.orientation }}" style="width:40px;height:40px;">
-            <div class="thumbnail" style="width:40px;height:40px;">
-              <div class="centered" style="width:40px;height:40px;">
-                <img src="{{ data.sizes.thumbnail.url }}" alt="{{ data.title }}" style="max-height:40px; width:auto;"/>
+      <li class="attachment selection selected save-ready">
+        <div class="attachment-preview type-image subtype-png {{ data.sizes.thumbnail.orientation }}" style="width:80px;height:80px;">
+            <div class="thumbnail">
+              <div class="centered">
+                <img src="{{ data.sizes.thumbnail.url }}" alt="{{ data.title }}" style="max-height:80px; width:auto;"/>
               </div>
             </div>
         </div>
@@ -101,15 +116,15 @@ class UW_Slideshow extends WP_Widget
 
     <?php  echo $before_widget; ?>
 
-      <?php foreach ( $ids as $index=>$id ) : $attachment = get_post( $id );  ?>
+      <?php foreach ( $ids as $index=>$id ) : $attachment = get_post( $id ); $link = get_post_meta( $attachment->ID, '_external_link', true ); ?>
 
         <div class="slide<?php echo ( $index == 0 ) ? ' active-slide' : ''; ?>">
 
         <?php echo wp_get_attachment_image( $id, 'original' ); ?>
 
           <div class="slide-info">
-            <h3><?php echo $attachment->post_title; ?></h3>
-            <?php echo wpautop($attachment->post_excerpt); ?>
+            <h3><a href="<?php echo $link ?>"><?php echo $attachment->post_title; ?></a></h3>
+            <?php echo wpautop( $attachment->post_excerpt ); ?>
           </div>
 
         </div>
@@ -129,4 +144,27 @@ class UW_Slideshow extends WP_Widget
     <?php echo $after_widget; 
 
   }
+
+  function external_link_to_field_edit( $form_fields, $post ) 
+  {
+		$form_fields['external_link'] = array(
+			'label' => 'External Link',
+			'input' => 'text',
+      'show_in_edit' => false,
+			'value' => get_post_meta( $post->ID, '_external_link', true )
+		);
+
+		return $form_fields;
+  }
+
+  function external_link_to_field_save( $post, $attachment ) 
+  {
+    $link = $attachment['external_link'];
+
+    if ( isset( $attachment['external_link'] ) ) 
+			update_post_meta( $post['ID'], '_external_link', $link );
+
+    return $post;
+  }
+
 }
