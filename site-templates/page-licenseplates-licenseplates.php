899423ca6dc6dbf6498de74e5d06c5508844def1
Kilian Frey <kilianf@uw.edu>
2013-11-25 13:14:02 -0800

    Added license plate site

---
+++ site-templates/page-licenseplates-licenseplates.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+		<div>
+			<div id="content" role="main" class="container">
+									
+			<div class="row show-grid">
+				<div class="span12">
+			
+					
+			<?php while ( have_posts() ) : the_post(); ?>
+
+      <span id="arrow-mark" <?php the_blogroll_banner_style(); ?> ></span>
+				
+      <?php uw_breadcrumbs(); ?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="entry-content">
+					<?php the_content(); ?>
+					<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'uw' ) . '</span>', 'after' => '</div>' ) ); ?>
+				</div><!-- .entry-content -->
+				<footer class="entry-meta">
+					<?php edit_post_link( __( 'Edit', 'uw' ), '<span class="edit-link">', '</span>' ); ?>
+				</footer><!-- .entry-meta -->
+			</article><!-- #post-<?php the_ID(); ?> -->
+
+					<?php comments_template( '', true ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+
+
+				</div>
+ 			 </div>
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_footer(); ?>
