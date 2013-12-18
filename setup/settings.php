4c98e7ad1c3c10164439daefaa4466df8aaab4d0
Dane Odekirk <dizzzy@uw.edu>
2013-12-18 13:54:34 -0800

    reenabling new user registration emails

--- setup/settings.php
+++ setup/settings.php
@@ -1,11 +1,5 @@
 <?php
 
-
 // Wordpress 3.5 oEmbed max-width
 if ( ! isset( $content_width ) ) 
   $content_width = 620;
-
-/**
- * Disable new user notification emails
- */
-add_filter( 'wpmu_signup_user_notification', '__return_false' );
